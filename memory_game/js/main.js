e9ed9e841d49aca383dc37032d527485d7265108
James Hankins <Hankins.JamesB@gmail.com>
2019-05-30 00:57:43 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,25 @@
 console.log("Up and running!");
 
-var cardOne = 'King';
-var cardTwo = 'King';
-var cardThree = 'Queen';
-var cardFour = 'Queen';
+var cards = ['queen', 'queen', 'king', 'king']
+var cardsInPlay = []
+
+var cardOne = cards[0]
+var cardTwo = cards[2]
+
+cardsInPlay.push(cardOne)
+cardsInPlay.push(cardTwo)
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("user flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] == cardsInPlay[1]) {
+		alert("You found a match!")
+		}
+	else {
+		alert("Sorry, try again.")
+		}
+	} 
+else {
+	console.log("cards aren't at 2");
+	}
